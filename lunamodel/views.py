08c8c9307462751858a2555124dd950e5ea0ec99
kimhaeil <kheail87@gmail.com>
2022-05-07 06:06:32 +0900

    model 사용없이 rest framwork 이용

--- lunamodel/views.py
+++ lunamodel/views.py
@@ -4,6 +4,11 @@ from django.db import connection
 import json
 from django.http import JsonResponse
 from django.core import serializers
+
+from rest_framework.decorators import api_view
+from .serializers import StoresSerializer
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 
@@ -19,8 +24,15 @@ def MainView(articleTag, id=None):
     '''
 
 
+@api_view(['GET', 'POST'])
+@csrf_exempt
 def getStores(request):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM [BikeStores].[sales].[stores]")
-        row = cursor.fetchall()
-        return JsonResponse(row, safe=False, json_dumps_params={'ensure_ascii': False})
+
+    if request.method == 'GET':
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM [BikeStores].[sales].[stores]")
+            stores = cursor.fetchall()
+            serializer = StoresSerializer(stores, many=True).data
+
+        return JsonResponse(serializer, safe=False)
