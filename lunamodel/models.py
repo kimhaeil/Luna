f64ea7738eec16c109c00e617a346732c270071a
kimhaeil <kheail87@gmail.com>
2022-05-09 23:50:43 +0900

    model 추가

--- lunamodel/models.py
+++ lunamodel/models.py
@@ -1,4 +1,16 @@
+from calendar import Calendar
 from pydoc import classname
-from django.db import connection
+from django.db import models
 
 # Create your models here.
+
+
+class Stores(models.Model):
+    store_id = models.IntegerField()
+    store_name = models.CharField()
+    phone = models.CharField()
+    email = models.CharField()
+    street = models.CharField()
+    city = models.CharField()
+    state = models.CharField()
+    zip_code = models.CharField()
--- lunamodel/views.py
+++ lunamodel/views.py
@@ -1,15 +1,15 @@
+from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db import connection
 import json
 from django.http import JsonResponse
-from django.core import serializers
 
 from rest_framework.decorators import api_view
-from .serializers import StoresSerializer
 from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework.response import Response
 # Create your views here.
+from rest_framework.renderers import JSONRenderer
 
 
 def MainView(articleTag, id=None):
@@ -27,12 +27,12 @@ def MainView(articleTag, id=None):
 @api_view(['GET', 'POST'])
 @csrf_exempt
 def getStores(request):
-
     if request.method == 'GET':
         with connection.cursor() as cursor:
             cursor.execute(
                 "SELECT * FROM [BikeStores].[sales].[stores]")
             stores = cursor.fetchall()
-            serializer = StoresSerializer(stores, many=True).data
+            serializer = StoresSerializer(stores, many=True)
+            print(json)
 
-        return JsonResponse(serializer, safe=False)
+        return JsonResponse(serializer.data, safe=False)
