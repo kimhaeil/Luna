e5728c98d3c70faa7af2a0d1ad8ac679594a8314
kimhaeil <kheail87@gmail.com>
2022-05-11 22:39:56 +0900

    - react event 기능 테스트 - props 기능 테스트

--- lunaclient/src/App.js
+++ lunaclient/src/App.js
@@ -2,25 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function Header(){
+function Header(props){
     return  <header>
-                <h1><a href="/">Luna</a></h1>
+                <h1><a href="/" onClick={function(event){
+                  event.preventDefault();
+                  props.onChangeMode()
+                }}>Luna</a></h1>
             </header>
 }
 
-function Nav(){
+function Nav(props){
+  const brands = []
+  for(let i=0; i<props.items.length; i++){
+    let c = props.items[i];
+    brands.push(<li>
+                    <a id={c.id} href={'/read/'+c.id} onClick={event=>{
+                      event.preventDefault();
+                      props.onChangeMode(event.target.id);
+                    }} >{c.contents}</a>
+                </li>);
+  }
     return  <nav>
                 <ol>
-                    <li><a href="getStroes">Get Store</a></li>
+                   {brands}
                 </ol>
              </nav>
 }
 
 function App() {
+  const items = [
+    {id: 1, contents:'Nike'},
+    {id: 2, contents:'Adidasss'},
+    {id: 3, contents:'Puma'}
+  ]
   return (
     <div>
-        <Header></Header>
-        <Nav></Nav>
+        <Header onChangeMode = {function(){
+          alert("Header");
+        }}></Header>
+        <Nav items={items} onChangeMode={(id) => {
+          alert(id);
+        }}></Nav>
     </div>
   );
 }
