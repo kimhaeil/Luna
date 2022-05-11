af9992ab7f8ce2eeed0de4d3c7ac2196487cbae1
Harry <kheail87@gmail.com>
2022-05-11 17:27:04 +0900

    Component 추가

--- lunaclient/src/App.js
+++ lunaclient/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header(){
+    return  <header>
+                <h1><a href="/">Luna</a></h1>
+            </header>
+}
+
+function Nav(){
+    return  <nav>
+                <ol>
+                    <li><a href="getStroes">Get Store</a></li>
+                </ol>
+             </nav>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header></Header>
+        <Nav></Nav>
     </div>
   );
 }
