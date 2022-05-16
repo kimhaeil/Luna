90e34dc746c573706181c1877f586946dcf56a78
kimhaeil <kheail87@gmail.com>
2022-05-16 23:24:32 +0900

    MUI 이용 로그인 창 샘플 만들기 연습

--- lunaclient/src/App.js
+++ lunaclient/src/App.js
@@ -1,177 +1,71 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
-import axios from "axios";
 
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-
-function GetStores() {
-  axios
-    .get("http://127.0.0.1:8000/select/")
-    .then((Response) => {
-      console.log(Response.data);
-    })
-    .catch((Error) => {
-      console.log(Error);
-    });
-}
-
-function Article(props) {
-  const [open, setOpen] = useState(false);
-  return (
-    <article>
-        <Box
-          x={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            "& > *": {
-              m: 1,
-            },
-          }}
-        >
-          <ButtonGroup variant="text" aria-label="text button group">
-            <Button>Home</Button>
-            <Button onClick={()=>{
-              setOpen(true);
-            }}>Search</Button>
-          </ButtonGroup>
-          <Dialog open={open}>
-            <DialogTitle>Search</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                Here is search calender
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-            <Button variant="text" >Ok</Button>
-              <Button variant="text" onClick={()=> {setOpen(false);}} >Cancel</Button>
-            </DialogActions>
-          </Dialog>
-        </Box>
-    </article>
-  );
-}
-
-function Header(props) {
-  return (
-    <header>
-      <h1>
-        {" "}
-        <a
-          href="/"
-          onClick={function (event) {
-            event.preventDefault();
-            props.onChangeMode();
-          }}
-        >
-          {" "}
-          Luna{" "}
-        </a>
-      </h1>
-    </header>
-  );
-}
-
-function Nav(props) {
-  const brands = [];
-  for (let i = 0; i < props.items.length; i++) {
-    let c = props.items[i];
-    brands.push(
-      <li>
-        <a
-          id={c.id}
-          href={"/read/" + c.id}
-          onClick={(event) => {
-            event.preventDefault();
-            props.onChangeMode(Number(event.target.id));
-          }}
-        >
-          {" "}
-          {c.contents}{" "}
-        </a>{" "}
-      </li>
-    );
-  }
-  return (
-    <nav>
-      <ol> {brands} </ol>{" "}
-    </nav>
-  );
-}
+//#region  MUI
+import TextField from "@mui/material/TextField";
+import Avatar from "@mui/material/Avatar";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Box, Container, Grid, Link, Typography } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+//#endregion
 
 function App() {
-  const [mode, setMode] = useState("WELCOME");
-  const [id, setId] = useState(null);
-  const items = [
-    {
-      id: 1,
-      contents: "Nike",
-      model: "Neymar",
-    },
-    {
-      id: 2,
-      contents: "Adidasss",
-      model: "Sonny",
-    },
-    {
-      id: 3,
-      contents: "Puma",
-      model: "Park",
-    },
-  ];
-  GetStores();
-  let content = null;
-  if (mode === "WELCOME") {
-    content = (
-      <Article title="Welcome" body="Hello Luna">
-        {" "}
-      </Article>
-    );
-  } else if (mode === "READ") {
-    let body,
-      title = null;
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].id === id) {
-        body = items[i].contents;
-        title = items[i].model;
-      }
-    }
-    content = (
-      <Article title={title} body={body}>
-        {" "}
-      </Article>
-    );
-  }
-
   return (
-    <Container fixed>
-      <Header
-        onChangeMode={function () {
-          setMode("WELCOME");
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop:8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems:'center'
         }}
       >
-        {" "}
-      </Header>{" "}
-      <Nav
-        items={items}
-        onChangeMode={(_id) => {
-          setMode("READ");
-          setId(_id);
-        }}
-      >
-        {" "}
-      </Nav>{" "}
-      <GetStores> </GetStores> {content}{" "}
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <TextField
+          margin="normal"
+          label="Email Address"
+          required
+          fullWidth
+          name="email"
+          autoComplete
+        />
+        <TextField
+          margin="normal"
+          label="Password"
+          type="password"
+          required
+          fullWidth
+          name="password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          SIGN IN
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid>
+            <Link>Don't have an account? Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 }
