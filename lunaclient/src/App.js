05a2b1e59d3fa0bb546964263f96064f064db0d5
kimhaeil <kheail87@gmail.com>
2022-05-23 22:56:44 +0900

    차트 추가 / REST API Call fetch

--- lunaclient/src/App.js
+++ lunaclient/src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
 
 //#region  MUI
-import TextField from "@mui/material/TextField";
-import Avatar from "@mui/material/Avatar";
-import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import { Box, Container, Grid, Link, Typography } from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import { Container, Grid } from "@mui/material";
+import Paper from "@mui/material/Paper";
 
 //#endregion
 
@@ -17,8 +10,7 @@ import Dashboard from "./Dashboard";
 import Chart from "./Chart";
 import Summary from "./Summary";
 import DistributionChart from "./DistributionChart";
-
-
+import UsageChart from "./UsageChart";
 
 function App() {
   return (
@@ -28,20 +20,9 @@ function App() {
       style={{ background: "#f2f6fc" }}
     >
       <Dashboard />
+      {/* #Summary */}
       <Grid container spacing={3}>
-        <Grid item xs={12} md={8} lg={9}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 240,
-            }}
-          >
-            <Chart />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4} lg={3}>
+        <Grid item xs={2} md={4} lg={3}>
           <Paper
             sx={{
               p: 2,
@@ -53,9 +34,19 @@ function App() {
             <Summary />
           </Paper>
         </Grid>
-      </Grid>
-      <Grid Container spacing={3}>
-        <Grid item xs={12} md={8} lg={9}>
+          <Grid item xs={4} md={2} lg={6}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              <DistributionChart />
+            </Paper>
+          </Grid>
+        <Grid item xs='auto' md='auto' lg={9}>
           <Paper
             sx={{
               p: 2,
@@ -64,7 +55,19 @@ function App() {
               height: 240,
             }}
           >
-            <DistributionChart/>
+            <Chart />
+          </Paper>
+        </Grid>
+        <Grid item xs='auto' md='auto' lg={9}>
+        <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            <UsageChart />
           </Paper>
         </Grid>
       </Grid>
