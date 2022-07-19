70fee6b7c20879019aac72c0695246d1172e8afa
kimhaeil <kheail87@gmail.com>
2022-07-19 23:23:51 +0900

    로그인 폼 스타일 수정

--- lunaclient/src/pages/Auth/AuthForm.js
+++ lunaclient/src/pages/Auth/AuthForm.js
@@ -2,31 +2,36 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Button from "../../common/Button";
 import palette from '../../lib/styles/palette';
-
 const AuthFormBlock = styled.div`
-    h3{
-        margin:0;
-        margin-bottom: 1rem;
-        color: ${palette.gray[8]};
-    }    
+  h3 {
+    margin: 0;
+    color: ${palette.gray[8]};
+    margin-bottom: 1rem;
+  }
 `;
 
+/**
+ * 스타일링된 input
+ */
 const StyledInput = styled.input`
-        font-size: 1rem;
-        border: none;
-        border-bottom: 1px solid ${palette.gray[5]};
-        padding-bottom: 0.5rem;
-        outline: none;
-        width: 100%;
-        &:focus {
-        color: $oc-teal-7;
-        border-bottom: 1px solid ${palette.gray[7]};
-        }
-        & + & {
-        margin-top: 1rem;
-        }
+  font-size: 1rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[5]};
+  padding-bottom: 0.5rem;
+  outline: none;
+  width: 100%;
+  &:focus {
+    color: $oc-teal-7;
+    border-bottom: 1px solid ${palette.gray[7]};
+  }
+  & + & {
+    margin-top: 1rem;
+  }
 `;
 
+/**
+ * 폼 하단에 로그인 혹은 회원가입 링크를 보여줌
+ */
 const Footer = styled.div`
   margin-top: 2rem;
   text-align: right;
@@ -38,6 +43,8 @@ const Footer = styled.div`
     }
   }
 `;
+
+const ButtonWithMaringTop = styled(Button)`margin-top :5rem;`;
 
 
 const AuthForm = () =>{
@@ -52,7 +59,7 @@ const AuthForm = () =>{
                     placeholder="pw"
                     type="password"
                 />
-                <Button>Login</Button>
+                <ButtonWithMaringTop cyan fullWidth>Login</ButtonWithMaringTop>
             </form>
             <Footer>
                 <Link to="/register">Sing in</Link>
