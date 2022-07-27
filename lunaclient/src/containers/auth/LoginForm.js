5368ff6a8ea7f442c891c664d162812cc490ef5c
kimhaeil <kheail87@gmail.com>
2022-07-27 23:22:57 +0900

    일정관리 프로젝트 등록(연습01)

--- lunaclient/src/containers/auth/LoginForm.js
+++ lunaclient/src/containers/auth/LoginForm.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const LoginForm = () =>{
-    return (<AuthForm></AuthForm>);
+    const dispatch = useDispatch();
+    const {form} = useSelector(({auth}) =>({
+        form:auth.login
+    }));
+
+    const onChange = e =>{
+        const {value,name} =e.target;
+        
+    }
 }
 
 export default LoginForm;
