21a49402a921d1aa3a13713f9d222db59c638720
kimhaeil <kheail87@gmail.com>
2022-08-11 23:36:45 +0900

    페이지 전환 연습

--- lunaVol2/image-logger/src/App.js
+++ lunaVol2/image-logger/src/App.js
@@ -8,7 +8,8 @@ import Home from './pages/Home';
 function App() {
   return (
     <Routes>
-
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
     </Routes>
   );
 }
--- lunaVol2/image-logger/src/pages/Home.js
+++ lunaVol2/image-logger/src/pages/Home.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Home = () =>{
     return(
         <div>
             <h1>main page</h1>
+            <Link to="/about">About</Link>
         </div>
     )
 }
