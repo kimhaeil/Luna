7bda9daaeeeaae4d025eac503e855c5ebf0541ae
kimhaeil <kheail87@gmail.com>
2022-08-15 14:38:52 +0900

    개념학습

--- lunaVol2/image-logger/src/pages/About.js
+++ lunaVol2/image-logger/src/pages/About.js
@@ -1,9 +1,12 @@
-const About = () =>{
-    return(
-        <div>
-            <h1>about page</h1>
-        </div>
-    )
-}
+import * as React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const About = () => {
+  return (
+    <div>
+      <h1>about page</h1>
+    </div>
+  );
+};
 
 export default About;
