e83a721aa1a8802fc01cda1e2a62f191efcbb967
kimhaeil <kheail87@gmail.com>
2022-08-22 21:30:04 +0900

    django+postgresql  연동

--- lunaVol2/image-logger-svr/lunaVol2Svr/settings.py
+++ lunaVol2/image-logger-svr/lunaVol2Svr/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lunaVol2Svr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imageLogger',
+        'USER': 'postgres',
+        'PASSWORD': '5822511',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
