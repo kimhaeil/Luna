9635f2e60cf1a341bbbadbaa2a0065921940d8d6
kimhaeil <kheail87@gmail.com>
2022-08-24 23:14:20 +0900

    Postgresql 모델 생성

---
+++ lunaVol2/image-logger-svr/imageloggerapp/urls.py
@@ -0,0 +1,10 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+from imageloggerapp import views
+
+urlpatterns = [
+    path('', views.img_info)
+]
--- lunaVol2/image-logger-svr/imageloggerapp/views.py
+++ lunaVol2/image-logger-svr/imageloggerapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from ast import Try
+from django.shortcuts import render, HttpResponse
+from imageloggerapp.models import image_loagger
 
-# Create your views here.
+
+def img_info(request):
+    try:
+        file_name = image_loagger.objects.all()
+    except image_loagger.DoesNotExist:
+        file_name = None
+
+    print(file_name)
+    return HttpResponse(file_name)
