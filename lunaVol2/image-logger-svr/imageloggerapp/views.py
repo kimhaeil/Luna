63ecce1d75b630f3cd48034e974a4ae540ecdec8
kimhaeil <kheail87@gmail.com>
2022-09-27 23:01:26 +0900

    Django template 상속

--- lunaVol2/image-logger-svr/imageloggerapp/views.py
+++ lunaVol2/image-logger-svr/imageloggerapp/views.py
@@ -1,4 +1,5 @@
 from ast import Try
+from distutils.text_file import TextFile
 from django.shortcuts import render, HttpResponse
 from .models import ImageInfoModel
 from .serializers import ImageInfoSerializer
@@ -6,8 +7,11 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 
 from rest_framework.views import APIView
-from django.http import Http404
 
+from django.conf import settings
+import os
+from chartjs.views.lines import BaseLineChartView
+from django.views.generic import TemplateView
 
 class ImageList(APIView):
     def get(self, request):
@@ -15,3 +19,47 @@ class ImageList(APIView):
         print(images)
         serializer = ImageInfoSerializer(images, many=True)
         return Response(serializer.data)
+
+
+def mainpage(request):
+    file_ = open(os.path.join(settings.BASE_DIR, 'test.txt'))
+    print(file_.name)
+    f = open(file_.name, 'r')
+    if f.mode == 'r':
+        contents = f.read()
+        print(contents)
+    return HttpResponse()
+
+
+def post_list(request):
+
+    file_ = open(os.path.join(settings.BASE_DIR, 'test.txt'))
+    print(file_.name)
+    f = open(file_.name, 'r')
+    if f.mode == 'r':
+        contents = f.read()
+        print(contents)
+    return render(request, 'imageloggerapp/log_list.html', {
+        'post_list': contents
+    })
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+                
+line_chart = TemplateView.as_view(template_name='imageloggerapp/chart_list.html')
+line_chart_json = LineChartJSONView.as_view()
+ 
--- lunaVol2/image-logger-svr/lunaVol2Svr/settings.py
+++ lunaVol2/image-logger-svr/lunaVol2Svr/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from os import os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DIR: os.path.join(BASE_DIR, 'imageloggerapp', 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
-    'rest_framework'
+    'bootstrap4',
+    'rest_framework',
+    'chartjs'
 ]
 
 MIDDLEWARE = [
