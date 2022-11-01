f801453d13094201992d33707f96d1a825f7ead9
kimhaeil <kheail87@gmail.com>
2022-11-01 23:06:44 +0900

    django form-set 모델 추가

--- lunaVol2/image-logger-svr/imageloggerapp/forms.py
+++ lunaVol2/image-logger-svr/imageloggerapp/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.forms import BaseFormSet, formset_factory
 
-class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    file = forms.FileField()
+class ConfigurationForms(forms.Form):
+    date = forms.DateField(required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        raise forms.ValidationError("Error")
+        return cleaned_data
+
+class ContactBaseFormSet(ContactBaseFormSet):
+    def add_fields(self, form, index):
+        super().add_fields(form, index)
+
+    def clean(self):
+        super().clean()
+        raise forms.ValidationError("Error")
+
+ContactFormSet = formset_factory(ConfigurationForms, formset=ContactBaseFormSet, extra=2, max_num=4, validate_max=True)
